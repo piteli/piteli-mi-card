50dd81e0c6bcedd2fea93a7212127e6d423373c9
piteli <rushwender@gmail.com>
2020-06-03 07:16:53 +0800

    updated 3

--- lib/main.dart
+++ lib/main.dart
@@ -12,30 +12,20 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.teal,
             body : SafeArea(
               child : Column(
-                mainAxisAlignment: MainAxisAlignment.stretch ,
-                children : <Widget>[
-                  Container(
-                  height : 100,
-                  margin : EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
-                  padding : EdgeInsets.all(20.0),
-                  color : Colors.white,
-                  child : Text('Container 1')
-              ),
-                SizedBox(
-                  height : 20.0
-                ),
-                Container(
-                  height : 100,
-                    color : Colors.red,
-                    child : Text('Container 2')
-                ),
-                Container(
-                    height : 100,
-                    color : Colors.blue,
-                    child : Text('Container 3')
-                )
-              ]
-            )
+                  children : <Widget>[
+                    CircleAvatar(
+                        radius : 50.0,
+                        backgroundColor : Colors.red,
+                        backgroundImage : AssetImage('images/avatar_profile.png')
+                    ),
+                    Text('piteli chan',
+                          style : TextStyle(
+                            fontSize : 40.0,
+                            color : Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),)
+        ]
+              )
         )
     );
   }
