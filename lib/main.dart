02a5baf5464b747f7b8b50abac6e285edd1ea7ae
piteli <rushwender@gmail.com>
2020-06-06 23:43:24 +0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -33,52 +33,58 @@ class MyApp extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                           letterSpacing: 2.5
                           ),),
-                    Container(
-                      padding : EdgeInsets.all(10.0),
+                    Card(
+//                      padding : EdgeInsets.all(10.0),
                       color : Colors.white,
                       margin : EdgeInsets.symmetric(vertical : 10.0, horizontal: 25.0),
-                      child: Row(
-                        children: <Widget>[
-                          Icon(
-                              Icons.phone,
-                              color : Colors.white
-                          ),
-                          SizedBox(
-                            width : 10.0
-                          ),
-                          Text(
-                            '0191213123',
-                            style: TextStyle(
-                              color : Colors.teal.shade900,
-                              fontFamily : 'Source Sans Pro',
-                              fontSize : 20.0
+                      child:  Padding(
+                        padding: EdgeInsets.all(25.0),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                                Icons.phone,
+                                color : Colors.white
+                            ),
+                            SizedBox(
+                              width : 10.0
+                            ),
+                            Text(
+                              '0191213123',
+                              style: TextStyle(
+                                color : Colors.teal.shade900,
+                                fontFamily : 'Source Sans Pro',
+                                fontSize : 20.0
+                              )
                             )
-                          )
-                        ]
+                          ]
+                        ),
                       )
                     ),
-                    Container(
-                        padding : EdgeInsets.all(10.0),
+                    Card(
+//                        padding : EdgeInsets.all(10.0),
                         color : Colors.white,
                         margin : EdgeInsets.symmetric(vertical : 10.0, horizontal: 25.0),
-                        child: Row(
-                            children: <Widget>[
-                              Icon(
-                                  Icons.email,
-                                  color : Colors.white
-                              ),
-                              SizedBox(
-                                  width : 10.0
-                              ),
-                              Text(
-                                  'piteli@chan.com',
-                                  style: TextStyle(
-                                      color : Colors.teal.shade900,
-                                      fontFamily : 'Source Sans Pro',
-                                      fontSize : 20.0
-                                  )
-                              )
-                            ]
+                        child: Padding(
+                          padding: EdgeInsets.all(25.0),
+                          child: Row(
+                              children: <Widget>[
+                                Icon(
+                                    Icons.email,
+                                    color : Colors.white
+                                ),
+                                SizedBox(
+                                    width : 10.0
+                                ),
+                                Text(
+                                    'piteli@chan.com',
+                                    style: TextStyle(
+                                        color : Colors.teal.shade900,
+                                        fontFamily : 'Source Sans Pro',
+                                        fontSize : 20.0
+                                    )
+                                )
+                              ]
+                          ),
                         )
                     )
         ]
