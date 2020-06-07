83655916f869f146eb061d152c701e4ab715625c
piteli <rushwender@gmail.com>
2020-06-07 23:52:34 +0800

    updated 7

--- lib/main.dart
+++ lib/main.dart
@@ -33,58 +33,45 @@ class MyApp extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                           letterSpacing: 2.5
                           ),),
+                    SizedBox(height : 20.0, width : 150.0,
+                      child : Divider(
+                        color : Colors.teal.shade100
+                      )
+                    ),
                     Card(
 //                      padding : EdgeInsets.all(10.0),
                       color : Colors.white,
                       margin : EdgeInsets.symmetric(vertical : 10.0, horizontal: 25.0),
-                      child:  Padding(
-                        padding: EdgeInsets.all(25.0),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                                Icons.phone,
-                                color : Colors.white
-                            ),
-                            SizedBox(
-                              width : 10.0
-                            ),
-                            Text(
-                              '0191213123',
-                              style: TextStyle(
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.phone, color: Colors.teal
+                        ),
+                        title : Text(
+                            '0191213123',
+                            style: TextStyle(
                                 color : Colors.teal.shade900,
                                 fontFamily : 'Source Sans Pro',
                                 fontSize : 20.0
-                              )
                             )
-                          ]
-                        ),
+                        )
                       )
                     ),
                     Card(
-//                        padding : EdgeInsets.all(10.0),
+//                      padding : EdgeInsets.all(10.0),
                         color : Colors.white,
                         margin : EdgeInsets.symmetric(vertical : 10.0, horizontal: 25.0),
-                        child: Padding(
-                          padding: EdgeInsets.all(25.0),
-                          child: Row(
-                              children: <Widget>[
-                                Icon(
-                                    Icons.email,
-                                    color : Colors.white
-                                ),
-                                SizedBox(
-                                    width : 10.0
-                                ),
-                                Text(
-                                    'piteli@chan.com',
-                                    style: TextStyle(
-                                        color : Colors.teal.shade900,
-                                        fontFamily : 'Source Sans Pro',
-                                        fontSize : 20.0
-                                    )
+                        child: ListTile(
+                            leading: Icon(
+                                Icons.email, color: Colors.teal
+                            ),
+                            title : Text(
+                                'Piteli Chan',
+                                style: TextStyle(
+                                    color : Colors.teal.shade900,
+                                    fontFamily : 'Source Sans Pro',
+                                    fontSize : 20.0
                                 )
-                              ]
-                          ),
+                            )
                         )
                     )
         ]
